698d2bfc1338d1a2f16652fb91e913d315ca0f1c
EC2 Default User <ec2-user@ip-10-0-0-207.ec2.internal>
2021-03-15 07:58:26 +0000

    apollonfq

--- src/Component/Aiml.js
+++ src/Component/Aiml.js
@@ -308,6 +308,54 @@ function SpeechToText(props) {
 }
 
 
+function TextToSpeech() {
+  const [response, setResponse] = useState("...")
+  const [textToGenerateSpeech, setTextToGenerateSpeech] = useState("write to speech");
+
+  function generateTextToSpeech() {
+    setResponse('Generating audio...');
+    Predictions.convert({
+      textToSpeech: {
+        source: {
+          text: textToGenerateSpeech,
+        },
+        voiceId: "Amy" // default configured on aws-exports.js 
+        // list of different options are here https://docs.aws.amazon.com/polly/latest/dg/voicelist.html
+      }
+    }).then(result => {
+      let AudioContext = window.AudioContext || window.webkitAudioContext;
+      console.log({ AudioContext });
+      const audioCtx = new AudioContext(); 
+      const source = audioCtx.createBufferSource();
+      audioCtx.decodeAudioData(result.audioStream, (buffer) => {
+
+        source.buffer = buffer;
+        source.connect(audioCtx.destination);
+        source.start(0);
+      }, (err) => console.log({err}));
+
+      setResponse(`Generation completed, press play`);
+    })
+      .catch(err => setResponse(err))
+  }
+
+  function setText(event) {
+    setTextToGenerateSpeech(event.target.value);
+  }
+
+  return (
+    <div className="Text">
+      <div>
+        <h3>Text To Speech</h3>
+        <input value={textToGenerateSpeech} onChange={setText}></input>
+        <button onClick={generateTextToSpeech}>Text to Speech</button>
+        <h3>{response}</h3>
+      </div>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
@@ -316,6 +364,10 @@ function App() {
       <br/>
        Speech To Text
       <SpeechToText />
+      <br />	  
+      <br />  
+      Text to Speech
+      <TextToSpeech />
       <br />	  
     </div>
   );
